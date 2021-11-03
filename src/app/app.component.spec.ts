b894637a1bf05c18b00a28883036e977ced7518d
Raghad Hav <raghad.hav@gmail.com>
2021-11-03 09:00:55 +0300

    Add the test for the form  and sending reques

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,18 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClientModule, HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient, HttpResponse, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Data } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>
+  let app: AppComponent;
+
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, HttpClientModule, FormsModule],
@@ -14,48 +21,140 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    app = fixture.componentInstance;
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'Loan Calculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('Loan Calculator');
   });
-  it(`All input field is non-empty`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    let submitBtn = document.getElementById("#saveBtn")
-   
-    app.enabled = false;
-    fixture.detectChanges();
-    // expect(submitBtn).toEqual('Loan Calculator');
+
+  it('Test SubmitFunction', () => {
+    const formData = <NgForm>{
+      value: {
+        monthlyIncome: 500000,
+        requestedAmount: 20000000,
+        loanTerm: 36,
+        children: 'NONE',
+        coapplicant: 'NONE'
+      }
+    };
+    const responseData = {
+      interestRate: 2500,
+      loanAmount: 20000000
+    };
+
+    app.SubmitFunction(formData);
+    const req = httpTestingController.expectOne(app.targetUrl);
+    req.flush(responseData);
+    expect(app.showBtn).toBeTrue();
   });
 
-});
+  it('Sending wrong data', () => {
+    const submittedData = {
+      monthlyIncome: -1,
+      requestedAmount: -1,
+      loanTerm: -1,
+      children: 'NONE',
+      coapplicant: 'NONE'
+    };
 
-describe('HttpClient testing', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, HttpClientModule, FormsModule],
-      providers: [AppComponent],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-    
+    const responseData = { status: 404 }
+
+    httpClient.post<Data>(app.targetUrl, submittedData, app.httpOptions)
+      .subscribe(data => console.log(data));
+
+    const req = httpTestingController.expectOne(app.targetUrl);
+    expect(req.request.method).toEqual('POST');
+    req.flush(responseData);
+
+    httpTestingController.verify();
   });
-  it('Test the post request', () => {
-    const responseData = { interestRate: 2500, loanAmount: 20000000 }
-    const sentData = {
-      monthlyIncome: 500000, requestedAmount: 20000000,
-      loanTerm: 36, children: "NONE", coapplicant: "NONE"
-    }
-  
+
+  it('Should Submit the data and return the Loan Amount and Interest Rate', () => {
+    const submittedData = {
+      monthlyIncome: 500000,
+      requestedAmount: 20000000,
+      loanTerm: 36,
+      children: 'NONE',
+      coapplicant: 'NONE'
+    };
+
+    const responseData = {
+      interestRate: 2500,
+      loanAmount: 20000000
+    };
+
+    httpClient.post<Data>(app.targetUrl, submittedData, app.httpOptions)
+      .subscribe(data => expect(data).toEqual(responseData));
+
+    const req = httpTestingController.expectOne(app.targetUrl);
+    expect(req.request.method).toEqual('POST');
+    req.flush(responseData);
+
+    httpTestingController.verify();
+  });
+
+  it('Should create a form with 3 inputs fields and 2 select', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('input[name="monthlyIncomeField"]')).toBeTruthy();
+    expect(compiled.querySelector('input[name="requestedAmountField"]')).toBeTruthy();
+    expect(compiled.querySelector('input[name="loanTermField"]')).toBeTruthy();
+    expect(compiled.querySelector('select[name="childrenField"]')).toBeTruthy();
+    expect(compiled.querySelector('select[name="coapplicantField"]')).toBeTruthy();
+  });
+
+  it('Should render the button "save"', () => {
+    const compiled = fixture.debugElement.query(By.css('#saveBtn'));
+    expect(compiled).toBeTruthy();
   })
+
+  it('Should have a method "SubmitFunction()"', () => {
+    const app = fixture.debugElement.componentInstance;
+    expect(app.SubmitFunction).not.toBe(undefined);
+  });
+
+  it('Should call the "SubmitFunction()" when the "save" button is clicked',
+    fakeAsync(() => {
+      const sfSpy = spyOn(app, 'SubmitFunction');
+      fixture.detectChanges();
+      let btn = fixture.debugElement.query(By.css('#saveBtn'));
+      btn.nativeElement.click();
+      tick();
+      fixture.detectChanges();
+      expect(sfSpy).toHaveBeenCalled();
+    })
+  );
+
+  it('Check SubmitFunction() is called when form submitted', fakeAsync(() => {
+    const sfSpy = spyOn(app, 'SubmitFunction');
+    fixture.detectChanges();
+    let form = fixture.debugElement.query(By.css('form'));
+
+    const fakeEvent = (e: any) => {
+      e.preventDefault();
+    }
+
+    form.triggerEventHandler('submit', fakeEvent);
+    tick();
+    fixture.detectChanges();
+    expect(sfSpy).toHaveBeenCalled();
+
+  }));
+
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 });
+
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,51 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   constructor(private http: HttpClient) { }
   @Input() enabled = true;
+
   title = 'Loan Calculator';
   loanAmount = 0;
   interestRate = 0;
   errorMessege = '';
   showBtn = false;
+  targetUrl = 'http://localhost:4200/api'
 
-SubmitFunction(calculateLoanForm: NgForm) {
-   this.showBtn = true;
-    console.log("calculateLoanForm.value.childrenField", calculateLoanForm.value.childrenField)
-    const headers = { 'X-API-KEY': 'swb-222222', 'Content-Type': 'application/json' }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'X-API-KEY': 'swb-222222'
+    })
+  };
+
+  SubmitData(body: any) {
+    return this.http.post<any>(this.targetUrl, body, this.httpOptions)
+    .pipe(catchError(this.handleError))
+  }
+
+  SubmitFunction(calculateLoanForm: NgForm) {
+    this.showBtn = true;
+    // console.log("calculateLoanForm.value.childrenField", calculateLoanForm.value.childrenField)
     const body = {
-      "monthlyIncome": calculateLoanForm.value.monthlyIncomeField, "requestedAmount": calculateLoanForm.value.requestedAmountField,
-      "loanTerm": calculateLoanForm.value.loanTermField, "children": calculateLoanForm.value.childrenField, "coapplicant": calculateLoanForm.value.coapplicantField
+      "monthlyIncome": calculateLoanForm.value.monthlyIncomeField, 
+      "requestedAmount": calculateLoanForm.value.requestedAmountField,
+      "loanTerm": calculateLoanForm.value.loanTermField, 
+      "children": calculateLoanForm.value.childrenField, 
+      "coapplicant": calculateLoanForm.value.coapplicantField
     }
 
-    this.http.post<any>('http://localhost:4200/api', body, { headers: headers }).pipe(catchError(this.handleError)).subscribe(data => {
+    return this.SubmitData(body).subscribe(data => {
       console.log("data", data)
       this.loanAmount = data.loanAmount;
       this.interestRate = data.interestRate;
@@ -35,6 +53,7 @@ SubmitFunction(calculateLoanForm: NgForm) {
       this.errorMessege = error;
     });
   }
+
   private handleError(error: HttpErrorResponse) {
     let errorMessege = '';
     if (error.status === 0) {
@@ -43,8 +62,7 @@ SubmitFunction(calculateLoanForm: NgForm) {
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+      console.error(`Backend returned code ${error.status}, body was: `, error.error);
       errorMessege = `Backend returned code ${error.status}, body was: `, error.error;
     }
     errorMessege += ' Something bad happened; please try again later.';
